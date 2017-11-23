77640c227a746b9be1782230970640fedf47df59
kimdahlberg <dahlberg.kim@hotmail.com>
2017-11-23 14:11:06 +0100

    update

--- 6_customers_company_id_all.php
+++ 6_customers_company_id_all.php
@@ -25,28 +25,3 @@ if (!empty($customer)) {
     header("HTTP/1.0 404 Not Found");
     echo json_encode(["message" => "Customer not found"]);
 }
-
-
-/*
-$id = $_GET ['company_id'];
-
-$sql = 'SELECT * FROM customers WHERE company_id = "' . $id . '"';
-$stn = $pdo->query($sql);
-$stn->execute();
-$customers = $stn->fetchAll();
-foreach ($customers as $key => $value){
-    $sql = "SELECT * FROM address WHERE customer_id = ".$value['id'];
-    $query = $pdo->query($sql);
-    $address = $query->fetch();
-
-    if ($address != null){
-        $customers [$key]['$address'] = $address;
-    }
-}
-if (!empty($customer)){
-    header("Content-Type: application/json")
-echo json_encode($customers);
-}
-
-header("Content-Type: application/json");
-echo json_encode($customers) */
